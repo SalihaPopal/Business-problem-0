6d7905ddc2cc757e9804538f6d4b186ed9a79e0d
Saliha Popal <salihapopal@gmail.com>
2023-09-26 14:13:32 +0100

    Add query for tables

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const { Pool } = require("pg");
-require("dotenv").config();
 const app = express();
+const { Client } = require("pg");
+require("dotenv").config();
+
 
 const cors = require('cors');
 app.use(cors({
@@ -15,7 +16,7 @@ app.use(cors({
 
 // Enable CORS for requests from 'http://localhost:3000'
 app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.set('Access-Control-Allow-Origin', '*', 'http://localhost:3000');
   next();
 });
 
@@ -29,14 +30,14 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
-const db = new Pool({
+const db = new Client({
   port: process.env.DB_PORT,
   host: process.env.DB_HOSTNAME,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DBDATABASE_NAME,
   server_url: process.env.SERVER_URL,
-  ssl: false
+  ssl: true
 });
 
 db.connect(function (err){
@@ -46,54 +47,75 @@ db.connect(function (err){
   console.log("Connected to the database");
 });
 
-app.get('/cors', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.send({ "msg": "This has CORS enabled 🎈" })
-  })
 
-app.get("/", function (request, response) {
-  response.send(
-    "Your server is listening."
-  );
+app.get("/Users", function (req, res) {
+  db.query("SELECT * FROM Users")
+    .then((result) => {
+      res.status(200).json({ Users: result.rows });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+
+app.get("/Sessions", (req, res) => {
+  let claimedSession;
+
+  // Find an available session and claim it
+  db.query(
+    "UPDATE Sessions SET Session_Status = 'Claimed' WHERE SessionID = (SELECT SessionID FROM Sessions WHERE Session_Status = 'Available' AND (Is_Morning = true OR Is_Evening = true) LIMIT 1) RETURNING *;"
+  )
+    .then((result) => {
+      // Check if a session was successfully claimed
+      if (result.rows.length > 0) {
+        claimedSession = result.rows[0];
+        return db.query("SELECT * FROM Users");
+      } else {
+        return Promise.reject("No available sessions to claim.");
+      }
+    })
+    .then((userResult) => {
+      res.status(200).json({
+        Users: userResult.rows,
+        ClaimedSession: claimedSession,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: "An error occurred." });
+    });
 });
 
-app.get("/Users", function (request, response) {
-  response.json(
-    { Users}
-  );
-});
+// app.get('/Sessions/claim-session', (req, res) => {
+//   console.log("hi")
+// });
 
 
-// app.get("/Users", (req, res) => {
-//   let claimedSession;
+// app.post('/Sessions/claim-session', async (req, res) => {
+//   const { SessionID } = req.body;
 
-//   // Find an available session and claim it
-//   Client.query(
-//     "UPDATE Sessions SET Session_Status = 'Claimed' WHERE SessionID = (SELECT SessionID FROM Sessions WHERE Session_Status = 'Available' AND (Is_Morning = true OR Is_Evening = true) LIMIT 1) RETURNING *;"
-//   )
-//     .then((result) => {
-//       // Check if a session was successfully claimed
-//       if (result.rows.length > 0) {
-//         claimedSession = result.rows[0];
-//         return Client.query("SELECT * FROM Users");
-//       } else {
-//         return Promise.reject("No available sessions to claim.");
-//       }
-//     })
-//     .then((userResult) => {
-//       res.status(200).json({
-//         Users: userResult.rows,
-//         ClaimedSession: claimedSession,
-//       });
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       res.status(500).json({ error: "An error occurred." });
-//     });
+//   try {
+//     // Check if the session is available
+//     const sessionQuery = 'SELECT * FROM Sessions WHERE SessionID = $1 AND Session_Status = $2';
+//     const sessionResult = await db.query(sessionQuery, [SessionID, 'Available']);
+
+//     if (sessionResult.rowCount === 0) {
+//       return res.status(400).json({ message: 'Session not available for claiming.' });
+//     }
+
+//     // Claim the session by updating its status and associating it with the volunteer
+//     const claimQuery = 'UPDATE Sessions SET Session_Status = $1, VolunteerID = $2 WHERE SessionID = $3';
+//     await db.query(claimQuery, ['Claimed', volunteerID, SessionID]);
+
+//     res.status(200).json({ message: 'Session claimed successfully.' });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'An error occurred while claiming the session.' });
+//   }
 // });
 
 
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+
+
+
